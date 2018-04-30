21713cd4f1add44c538c258e857c9dea4cb0dc1b
David Zambrano <dzambrano.226@gmail.com>
2018-04-29 23:08:47 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+// Inicializando la App
+var MODULE = 'front-app', // DEJAR CON EL MISMO NOMBRE
+    API = {
+        'token_name': 'TOKEN-DIGIFUS', // EL NOMBRE DEL TOKEN QUE HAN DEFINIDO PARA SU API
+        'base_url': 'http://localhost/digifusApi/public/' // LA URL DE SU API
+    };
+
+var frontApp = angular.module(MODULE, [
+    'ngRoute',
+    'authControllers',
+    'solicitudControllers',
+    'perfilControllers',
+    'terceroControllers',
+    'testControllers'
+]);
+
+(function(){
+    if(typeof(localStorage[API.token_name]) === 'undefined'){
+        localStorage[API.token_name] = '';
+    }
+}());
---
+++ router.js
@@ -0,0 +1,38 @@
+// Rutas
+frontApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/auth/login.html',
+        controller: 'AuthLoginCtrl'
+      }).
+      when('/logout', {
+        templateUrl: 'partials/empty.html',
+        controller: 'AuthLogoutCtrl'
+      }).
+
+      when('/principal', {
+        templateUrl: 'partials/solicitudes/principal.html',
+        controller: 'solicitudListarCtrl'
+      }).
+      when('/principal/registrar', {
+        templateUrl: 'partials/solicitudes/registrar.html',
+        controller: 'solicitudRegistrarCtrl'
+      }).
+      when('/principal/visualizar/:id',{
+        templateUrl: 'partials/solicitudes/visualizar.html',
+        controller: 'SolicitudVisualizarCtrl'
+      }).
+      when('/test', {
+        templateUrl: 'partials/test/test.html',
+        controller: 'TestCtrl'
+      }).
+      when('/perfil/visualizar/:usuario',{
+          templateUrl: 'partials/perfil/visualizar.html',
+          controller: 'PerfilVisualizarCtrl'
+      }).
+
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
